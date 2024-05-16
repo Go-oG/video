c9961a59eea045da1c44ce18e4464301c87559fa
ggg <>
2024-05-16 17:25:50 +0800

    完成Demo

--- video/src/main/res/raw/demo.glsl
+++ video/src/main/res/raw/demo.glsl
@@ -1,36 +1,11 @@
 #version 100
 
 precision mediump float;
-
 varying highp vec2 vTextureCoord;
-uniform sampler2D sTexture;
-
-uniform float topLeftRadius;
-uniform float topRightRadius;
-uniform float bottomLeftRadius;
-uniform float bottomRightRadius;
-uniform vec2 mResolution;
-
-const vec2 tl = vec2(0.0, 1.0);
-const vec2 tr = vec2(1.0, 1.0);
-const vec2 bl = vec2(0.0, 0.0);
-const vec2 br = vec2(1.0, 0.0);
-
+varying highp vec2 vTextureCoord2;
+uniform lowp sampler2D sTexture;
 void main() {
-    vec2 coord = gl_FragCoord.xy / mResolution;
-    vec4 color = texture2D(sTexture, vTextureCoord);
-
-    float distTopLeft = distance(coord, tl) * mResolution.y;
-    float distTopRight = distance(coord, tr) * mResolution.y;
-    float distBottomLeft = distance(coord, bl) * mResolution.y;
-    float distBottomRight = distance(coord, br) * mResolution.y;
-
-    if (distTopLeft > topLeftRadius &&
-    distTopRight > topRightRadius &&
-    distBottomLeft > bottomLeftRadius &&
-    distBottomRight > bottomRightRadius) {
-        discard;
-    } else {
-        gl_FragColor = color;
-    }
+    vec4 color1 = texture2D(sTexture, vTextureCoord);
+    vec4 color2 = texture2D(sTexture, vTextureCoord2);
+    gl_FragColor = vec4(color1.rgb, color2.r);
 }
